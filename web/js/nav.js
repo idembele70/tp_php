4192b3196fd07491b435301df4be0f023e7b9c06
idembele70 <idembele70@gmail.com>
2021-04-06 00:03:07 +0200

    gestion d'affichage avancé des article

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,4 +1,29 @@
-let controlsNav = document.querySelector('nav');
-controlsNav.addEventListener('click', () => {
-controlsNav.classList.toggle('enabled');
-}, false);
+// selector & variable
+const controlsNav = document.querySelector('nav');
+let row;
+
+// event listener
+controlsNav.addEventListener('click', toggleNav, false);
+
+// function
+function enable(btn = controlsNav) {
+    btn.classList.toggle('btn-toggle')
+     row = document.querySelector('div > .row')
+    const btnClone = btn.parentNode.cloneNode(true);
+    btnClone.classList.toggle("article-toggle");
+    row.classList.toggle('row-toggle');
+    document.body.appendChild(btnClone);
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    span.setAttribute('onClick', 'enabled(this)')
+    btnClone.appendChild(span);
+    btn.parentElement.style.display = 'initial';
+}
+function enabled(btn) {
+    row.classList.toggle('row-toggle');
+    btn.parentElement.style.display = 'none';
+    console.log(this.lastChild);
+}
+function toggleNav() { controlsNav.classList.toggle('enabled'); }
